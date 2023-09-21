3b5b6d9d77c6f6004643a640de8b40ce9fc7400b
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2023-09-21 07:52:29 +0530

    feat: used JSX & learnt about component composition

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//                                    for single element
-// const heading = React.createElement(
-//   "h1",
-//   //tag
-//   { id: "heading" },
-//   //attributes
-//   "Hello world from react!"
-//   //children
-// );
-// // CDN link for react is getting used here
+const rawReactElement = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Raw React Element"
+);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// // CDN link for react Dom is getting used here
+//JSX and different , JSX is sugar syntax for JS which is not necessary for react
+const JSXelement = <h1 className="className">JSX element</h1>;
+// Proof that JSX is not HTML is the attribute we pass in JSX -> className not class, tabIndex not tabindex
+// JSX is not html inside javascript, JSX is HTML or XML like syntax
+// (JSX) like syntax it looks html but it is not HTML
+// JSX is not valid JS because browser cant undeerstand it directly
+// JSX also handles cross site scripting which is type of an attack on api to make response the mallicious
 
-// root.render(heading);
-// // root takes object turns into tag and puts into root.
+const FunctionalComponent = () => {
+  return (
+    <div>
+      {JSXelement}
+      {rawReactElement}
+    </div>
+  );
+};
 
-// console.log(heading, "<- it returns a object not an html tag");
+const elementWhichHaveFunctionalComp = (
+  <div>
+    element which have func comp
+    <FunctionalComponent />
+  </div>
+);
 
-//                                    for nested elements
-const Parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "children" }, [
-    React.createElement(
-      "h1",
-      { className: "headings" },
-      "this is namaste react"
-    ),
-    React.createElement("h1", { className: "headings" }, "second element"),
-    React.createElement("h1", { className: "headings" }, "third element"),
-  ]),
-  React.createElement("div", { id: "children2" }, [
-    React.createElement("h1", { className: "headings" }, "first element"),
-    React.createElement("h1", { className: "headings" }, "second element"),
-    React.createElement("h1", { className: "headings" }, "third element"),
-  ]),
-]);
-// to avoid these mess we use JSX
+const SecondFuntionComponent = () => {
+  const anyNumber = 1000;
+  return (
+    <div>
+      <FunctionalComponent />
+      {elementWhichHaveFunctionalComp}
+      <h1>component for component composition</h1>
+      {anyNumber} {/* {} <- this is ternary */}
+      {console.log(
+        "any JS code can run under ternary brackets even console.log"
+      )}
+      {/* this is component composition */}
+      {/* component composition is basically component used inside another component it can be also component passed as PROP */}
+    </div>
+  );
+};
+
+{
+  {
+    {
+      // testing bracket pair colorization
+    }
+  }
+}
+
+const ThirdFunctionComponent = () => SecondFuntionComponent();
+
+// parcel(our installed bundler) is uses babel to transpile the JSX element to normal JS before it reaches the JS Engine
+// babel transpile JSX before render
+// babel is package which is currently transitive dependency
+// babel is not a library
+// babel helps in older browser runs too
+// babel also does many thing apart from this
 
 const Root = ReactDOM.createRoot(document.getElementById("root"));
 
-Root.render(Parent);
+// babel jobs steps (babel is super hero of browser bro)
+// JSX -> React.createElement -> JS object -> rendered to make html element
 
-console.log(Parent, "<- it returns a object not an html tag");
+Root.render(<ThirdFunctionComponent />);
+
+// Root.render(ThirdFunctionComponent);
+// above example is for wrong rendering since Functional component have to be sent in HTML like syntax
+
+console.log(
+  JSXelement,
+  rawReactElement,
+  " <- both JSX element & react element return JS object and not html tag"
+);
